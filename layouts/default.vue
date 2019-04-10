03ed3c4eebed458b44e92bac6563771cba16bf7c
dora1998 <dorayaki.app@gmail.com>
2019-04-11 02:32:13 +0900

    最低限のレイアウト

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,20 +1,40 @@
-<template>
-  <div>
-    <nuxt />
-  </div>
+<template lang="pug">
+  div.layout_container
+    NameContainer.name
+    nuxt.page_container
 </template>
 
-<style>
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator'
+import NameContainer from '~/containers/NameContainer.vue'
+
+@Component({
+  components: {
+    NameContainer
+  }
+})
+export default class Index extends Vue {}
+</script>
+
+<style lang="scss">
+.layout_container {
+  @include mq(md) {
+    .page_container {
+      margin-left: 320px;
+    }
+  }
+}
+
 html {
-  font-family: 'Source Sans Pro', -apple-system, BlinkMacSystemFont, 'Segoe UI',
-    Roboto, 'Helvetica Neue', Arial, sans-serif;
   font-size: 16px;
-  word-spacing: 1px;
   -ms-text-size-adjust: 100%;
   -webkit-text-size-adjust: 100%;
   -moz-osx-font-smoothing: grayscale;
   -webkit-font-smoothing: antialiased;
   box-sizing: border-box;
+}
+body {
+  margin: 0;
 }
 
 *,
@@ -22,34 +42,5 @@ html {
 *:after {
   box-sizing: border-box;
   margin: 0;
-}
-
-.button--green {
-  display: inline-block;
-  border-radius: 4px;
-  border: 1px solid #3b8070;
-  color: #3b8070;
-  text-decoration: none;
-  padding: 10px 30px;
-}
-
-.button--green:hover {
-  color: #fff;
-  background-color: #3b8070;
-}
-
-.button--grey {
-  display: inline-block;
-  border-radius: 4px;
-  border: 1px solid #35495e;
-  color: #35495e;
-  text-decoration: none;
-  padding: 10px 30px;
-  margin-left: 15px;
-}
-
-.button--grey:hover {
-  color: #fff;
-  background-color: #35495e;
 }
 </style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,70 +1,19 @@
-<template>
-  <section class="container">
-    <div>
-      <logo />
-      <h1 class="title">
-        minoru.dev
-      </h1>
-      <h2 class="subtitle">
-        My Portfolio Website
-      </h2>
-      <div class="links">
-        <a href="https://nuxtjs.org/" target="_blank" class="button--green">
-          Documentation
-        </a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          class="button--grey"
-        >
-          GitHub
-        </a>
-      </div>
-    </div>
-  </section>
+<template lang="pug">
+  section.container
+    div
+      h1.title
+        | minoru.dev
 </template>
 
 <script lang="ts">
 import { Component, Vue } from 'vue-property-decorator'
-import Logo from '~/components/Logo.vue'
 
-@Component({
-  components: {
-    Logo
-  }
-})
+@Component({})
 export default class Index extends Vue {}
 </script>
 
-<style>
-.container {
-  margin: 0 auto;
-  min-height: 100vh;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  text-align: center;
-}
-
-.title {
-  font-family: 'Quicksand', 'Source Sans Pro', -apple-system, BlinkMacSystemFont,
-    'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif;
-  display: block;
-  font-weight: 300;
-  font-size: 100px;
-  color: #35495e;
-  letter-spacing: 1px;
-}
-
-.subtitle {
-  font-weight: 300;
-  font-size: 42px;
-  color: #526488;
-  word-spacing: 5px;
-  padding-bottom: 15px;
-}
-
-.links {
-  padding-top: 15px;
+<style lang="scss" scoped>
+h1 {
+  border-bottom: 1px solid #ccc;
 }
 </style>
