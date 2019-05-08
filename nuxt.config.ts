ba2e0ddf311babac56fefa2e2dc5a2862832598f
dora <m5i2n2o@gmail.com>
2019-05-09 02:30:09 +0900

    OGP, Autoprefixerの設定

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -8,11 +8,33 @@ const nuxtConfig: NuxtConfiguration = {
    ** Headers of the page
    */
   head: {
-    title: pkg.name,
+    title: "Minoru's Portfolio",
+    htmlAttrs: {
+      prefix: 'og: http://ogp.me/ns#'
+    },
+    titleTemplate: "%s - Minoru's Portfolio",
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: pkg.description }
+      {
+        hid: 'description',
+        name: 'description',
+        content: "Minoru Takeuchi's Portfolio Website"
+      },
+      {
+        hid: 'og:site_name',
+        property: 'og:site_name',
+        content: "Minoru's Portfolio"
+      },
+      { hid: 'og:type', property: 'og:type', content: 'website' },
+      { hid: 'og:url', property: 'og:url', content: 'https://minoru.dev' },
+      { hid: 'og:title', property: 'og:title', content: "Minoru's Portfolio" },
+      {
+        hid: 'og:description',
+        property: 'og:description',
+        content: "Minoru Takeuchi's Portfolio Website"
+      }
+      // { hid: 'og:image', property: 'og:image', content: 'https://example.com/img/ogp/common.jpg' },
     ],
     link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }]
   },
@@ -64,6 +86,14 @@ const nuxtConfig: NuxtConfiguration = {
           loader: 'eslint-loader',
           exclude: /(node_modules)/
         })
+      }
+    },
+
+    postcss: {
+      preset: {
+        autoprefixer: {
+          grid: true
+        }
       }
     }
   },
