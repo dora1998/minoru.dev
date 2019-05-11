98baa2a60e684755a0d7d4cdbb04c77402ef98ea
dora <m5i2n2o@gmail.com>
2019-05-11 18:54:22 +0900

    /worksの作成

---
+++ pages/works.vue
@@ -0,0 +1,21 @@
+<template>
+  <section class="container">
+    <div>
+      <h1 class="title">これまでの制作物</h1>
+    </div>
+  </section>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator'
+
+@Component({})
+export default class Works extends Vue {}
+</script>
+
+<style lang="scss" scoped>
+h1 {
+  margin-bottom: 8px;
+  border-bottom: 1px solid #ccc;
+}
+</style>
