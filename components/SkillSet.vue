dedd754459088c7138711a7432832e689e0f9032
dora <31735614+dora1998@users.noreply.github.com>
2020-01-19 17:59:57 +0900

    Implement skill pages

--- components/SkillSet.vue
+++ components/SkillSet.vue
@@ -1,12 +1,41 @@
 <template>
-  <section class="container"></section>
+  <div class="container">
+    <img v-for="s in skills" :key="s.name" :src="s.img" :alt="s.name" />
+  </div>
 </template>
 
 <script lang="ts">
-import { Component, Vue } from 'vue-property-decorator'
+import { Component, Prop, Vue } from 'vue-property-decorator'
 
+export interface Skill {
+  name: string
+  img: string
+}
 @Component({})
-export default class SkillSet extends Vue {}
+export default class SkillSet extends Vue {
+  @Prop({ default: [] }) readonly skills!: Skill[]
+}
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.container {
+  display: grid;
+  grid-auto-rows: auto;
+  grid-column-gap: 48px;
+  grid-row-gap: 32px;
+  grid-template-columns: repeat(auto-fit, 120px);
+  align-items: center;
+  justify-content: center;
+  margin: 32px 0;
+
+  @include mq(lg) {
+    padding: 0 64px;
+  }
+
+  img {
+    width: 100%;
+    object-fit: contain;
+    max-height: 64px;
+  }
+}
+</style>
--- pages/skills.vue
+++ pages/skills.vue
@@ -1,14 +1,24 @@
 <template>
   <main>
-    <h1 class="title">スキル</h1>
-    <SkillSet />
+    <section>
+      <h1 class="title">言語</h1>
+      <SkillSet :skills="langs" />
+    </section>
+    <section>
+      <h1 class="title">フレームワーク</h1>
+      <SkillSet :skills="frameworks" />
+    </section>
+    <section>
+      <h1 class="title">ツール等</h1>
+      <SkillSet :skills="tools" />
+    </section>
   </main>
 </template>
 
 <script lang="ts">
 import { Component, Vue } from 'vue-property-decorator'
 import { MetaInfo } from 'vue-meta'
-import SkillSet from '~/components/SkillSet.vue'
+import SkillSet, { Skill } from '~/components/SkillSet.vue'
 
 @Component({
   head(): MetaInfo {
@@ -21,7 +31,100 @@ import SkillSet from '~/components/SkillSet.vue'
     SkillSet
   }
 })
-export default class Skills extends Vue {}
+export default class Skills extends Vue {
+  langs: Skill[] = [
+    {
+      name: 'TypeScript',
+      img: require('~/assets/imgs/skills/ts.svg')
+    },
+    {
+      name: 'JavaScript',
+      img: require('~/assets/imgs/skills/js.svg')
+    },
+    {
+      name: 'Julia',
+      img: require('~/assets/imgs/skills/julia.svg')
+    },
+    {
+      name: 'Python',
+      img: require('~/assets/imgs/skills/python.svg')
+    },
+    {
+      name: 'Go',
+      img: require('~/assets/imgs/skills/go.svg')
+    },
+    {
+      name: 'C++',
+      img: require('~/assets/imgs/skills/cpp.svg')
+    },
+    {
+      name: 'Sass',
+      img: require('~/assets/imgs/skills/sass.svg')
+    }
+  ]
+
+  frameworks: Skill[] = [
+    {
+      name: 'Nuxt.js',
+      img: require('~/assets/imgs/skills/nuxt.svg')
+    },
+    {
+      name: 'Next.js',
+      img: require('~/assets/imgs/skills/nextjs.svg')
+    },
+    {
+      name: 'React',
+      img: require('~/assets/imgs/skills/react.svg')
+    },
+    {
+      name: 'Vue.js',
+      img: require('~/assets/imgs/skills/vue.svg')
+    },
+    {
+      name: 'jQuery',
+      img: require('~/assets/imgs/skills/jquery.svg')
+    },
+    {
+      name: 'Django',
+      img: require('~/assets/imgs/skills/django.svg')
+    }
+  ]
+
+  tools: Skill[] = [
+    {
+      name: 'Git',
+      img: require('~/assets/imgs/skills/git.svg')
+    },
+    {
+      name: 'Docker',
+      img: require('~/assets/imgs/skills/docker.png')
+    },
+    {
+      name: 'Webpack',
+      img: require('~/assets/imgs/skills/webpack.svg')
+    },
+    {
+      name: 'CircleCI',
+      img: require('~/assets/imgs/skills/circleci.png')
+    },
+    {
+      name: 'Netlify',
+      img: require('~/assets/imgs/skills/netlify.svg')
+    },
+    {
+      name: 'Jupyter',
+      img: require('~/assets/imgs/skills/jupyter.svg')
+    },
+    {
+      name: 'esa',
+      img: require('~/assets/imgs/skills/esa.svg')
+    },
+    {
+      name: 'Slack',
+      img: require('~/assets/imgs/skills/slack.svg')
+    }
+  ]
+}
 </script>
 
 <style lang="scss" scoped>
