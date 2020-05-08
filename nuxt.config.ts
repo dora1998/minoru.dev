dcb431c39b82b3b352f1e0fc06faa9231e1f9a81
dora <31735614+dora1998@users.noreply.github.com>
2020-05-08 20:07:45 +0900

    Revert "Add adsbygoogle"
    This reverts commit e739dffa17351d99a7638a7e75c35de52108b324.

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -39,14 +39,7 @@ const nuxtConfig: Configuration = {
       }
       // { hid: 'og:image', property: 'og:image', content: 'https://example.com/img/ogp/common.jpg' },
     ],
-    link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
-    script: [
-      {
-        src: 'https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js',
-        'data-ad-client': 'ca-pub-1234567890123456',
-        async: true
-      }
-    ]
+    link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }]
   },
 
   /*
